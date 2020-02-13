38b0136478c3f0e877de816ef63dafad7f5df3b7
Clonathan86 <forerojonathan@gmail.com>
2020-02-12 22:42:40 -0500

    Update App component from stateless to stateful

--- src/components/App.js
+++ src/components/App.js
@@ -1,54 +1,63 @@
 import React from 'react';
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: [
+        {
+          name: 'Tatán',
+          email: 'forerojonathan@gmail.com',
+          link: 'https://github.com/Clonathan86'
+        },
+        {
+          name: 'Platzi',
+          email: 'platzi@platzi.com',
+          link: 'https:/platzi.com'
+        }
+      ]
+    }
+  }
 
-const App = () => {
-
-  const setRows = () => [
-    <tr>
-      <td>
-        Tatán
-      </td>
-      <td>
-        forerojonathan@gmail.com
-      </td>
-      <td>
-        https://github.com/Clonathan86
-      </td>
-    </tr>,
-    <tr>
-      <td>
-        Platzi
-      </td>
-      <td>
-        platzi@platzi.com
-      </td>
-      <td>
-        https:/platzi.com
-      </td>
-    </tr>
-  ];
-
-  return (
-    <div className="margin">
-      <table className="table">
-        <thead>
-          <tr>
-            <th>
-              Name
-            </th>
-            <th>
-              Email
-            </th>
-            <th>
-              Link
-            </th>
-          </tr>
-        </thead>
-        <tbody>
-          { setRows() }
-        </tbody>
-      </table>
-    </div>
+  setRows = () => (
+    this.state.users.map((user) => (
+      <tr>
+        <td>
+          { user.name }
+        </td>
+        <td>
+        { user.email }
+        </td>
+        <td>
+        { user.link }
+        </td>
+      </tr>
+    ))
   );
+
+  render () {
+    return (
+      <div className="margin">
+        <table className="table">
+          <thead>
+            <tr>
+              <th>
+                Name
+              </th>
+              <th>
+                Email
+              </th>
+              <th>
+                Link
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            { this.setRows() }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default App;
