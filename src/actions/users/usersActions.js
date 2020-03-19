4b413156e84c41a485b896e7167faa346ae98d42
Clonathan86 <forerojonathan@gmail.com>
2020-03-18 20:01:23 -0500

    Add try catch inside users action

--- src/actions/users/usersActions.js
+++ src/actions/users/usersActions.js
@@ -2,9 +2,13 @@ import axios from 'axios';
 import * as ActionType from './types';
 
 export const getAll = () => async (dispatch) => {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
-    dispatch({
-        type: ActionType.GET_ALL_USERS,
-        payload: response.data
-    })
+    try {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+        dispatch({
+            type: ActionType.GET_ALL_USERS,
+            payload: response.data
+        })
+    } catch (error) {
+        console.log('Error;', error.message);
+    }
 }
