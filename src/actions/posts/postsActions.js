99eb85f4a6e061d7454830dd897c2c01a278964a
Clonathan86 <forerojonathan@gmail.com>
2020-04-05 18:38:03 -0500

    Update users reducer with the post list key

--- src/actions/posts/postsActions.js
+++ src/actions/posts/postsActions.js
@@ -1,5 +1,8 @@
 import axios from 'axios';
 import * as ActionType from '../posts/types';
+import * as UsuariosTypes from '../users/types';
+
+const { GET_ALL: USERS_GET_ALL } = UsuariosTypes;
 
 export const getByUser = (key) => async (dispatch, getState) => {
     const { users } = getState().usersReducer;
@@ -11,9 +14,27 @@ export const getByUser = (key) => async (dispatch, getState) => {
     });
     try {
         const response = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${user_id}`);
+
+        const updated_posts = [
+            ...posts,
+            response.data
+        ];
+
+        const posts_key = updated_posts.length - 1;
+        const updated_users = [...users];
+        updated_users[key] = {
+            ...users[key],
+            posts_key,
+        };
+
+        dispatch({
+            type: USERS_GET_ALL,
+            payload: updated_users
+        });
+
         dispatch({
             type: ActionType.GET_BY_USER,
-            payload: [...posts,response.data]
+            payload: updated_posts
         });
     } catch (error) {
         dispatch({
