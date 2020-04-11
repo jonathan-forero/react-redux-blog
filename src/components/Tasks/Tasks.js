4d70aef3c0b5e38a50341c9c3e701cc38702dbea
Clonathan86 <forerojonathan@gmail.com>
2020-04-11 15:11:29 -0500

    Mapping Objects

--- src/components/Tasks/Tasks.js
+++ src/components/Tasks/Tasks.js
@@ -1,17 +1,62 @@
 import React, { Component } from 'react';
+import Spinner from '../Spinner/Spinner';
+import Error from '../Error/Error';
 
 class Tasks extends Component {
 
   componentDidMount () {
-    this.props.getAll();
+    if (!Object.keys(this.props.tasks).length) {
+      this.props.getAll();
+    }
   }
+
+  showContent = () => {
+    const {
+      tasks,
+      loading,
+      error
+    } = this.props;
+
+    return (
+      loading ? <Spinner /> :
+        error ? <Error message={error} /> :
+          (
+            Object.keys(tasks).map(userId => (
+              <div key={userId}>
+                <h2>
+                  User {userId}
+                </h2>
+                <div className="tasks-container">
+                  { this.setTasks(userId) }
+                </div>
+              </div>
+            ))
+          )
+    )
+  }
+
+  setTasks = (userId) => {
+    const { tasks } = this.props;
+    const byUser = {
+      ...tasks[userId]
+    };
+
+    return Object.keys(byUser).map(taskId => (
+      <div key={taskId}>
+        <input type="checkbox" defaultChecked={byUser[taskId].completed} />
+        {
+          byUser[taskId].title
+        }
+      </div>
+    ));
+  };
 
   render() {
     console.log(this.props);
 
     return (
       <div>
-        Tasks Greeting!
+        { this.showContent() }
       </div>
     );
   }
