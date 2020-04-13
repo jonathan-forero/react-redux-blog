6557b4b7f566b3df9305fd4ea5d59398137d6769
Clonathan86 <forerojonathan@gmail.com>
2020-04-13 00:00:25 -0500

    Add Build

---
+++ build/precache-manifest.7fd0efd120476c806817f8c9c763cc28.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0933ceeef33ffaf9c81de514f49d5ed9",
+    "url": "/react-redux-blog/index.html"
+  },
+  {
+    "revision": "57d79d7bfc8c5ca0755c",
+    "url": "/react-redux-blog/static/css/main.3ccffaac.chunk.css"
+  },
+  {
+    "revision": "e74c484044f4c8990596",
+    "url": "/react-redux-blog/static/js/2.ce677206.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/react-redux-blog/static/js/2.ce677206.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57d79d7bfc8c5ca0755c",
+    "url": "/react-redux-blog/static/js/main.d739b521.chunk.js"
+  },
+  {
+    "revision": "f975c70d71d33ac3b81b",
+    "url": "/react-redux-blog/static/js/runtime-main.a372d5f2.js"
+  }
+]);
